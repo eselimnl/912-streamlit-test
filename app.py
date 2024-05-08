b6d8ce2321cbc81e01d6b4ae306e57eb229c4eca
eselimnl <eselimnl@gmail.com>
2024-05-08 22:58:36 +0100

    test23

--- app.py
+++ app.py
@@ -10,30 +10,36 @@ st.title('My ESU Streamlit app 2')
 user_input = st.text_input("Enter some text")
 st.write('The user entered:', user_input)
 
-GA_ID = "google_analytics"
-GA_SCRIPT = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-D39YXHSX0V"></script>
-<script id='google_analytics'>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-D39YXHSX0V');
+import streamlit.components.v1 as components
+
+google_analytics_js = """
+<script>
+function loadScript(url) {       
+    return new Promise(function(resolve, reject) {
+        // Add the script to the main page, not the component iframe
+        const doc = window.parent.document;
+        var head = doc.getElementsByTagName('head')[0];
+        var script = doc.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.async = true;
+        script.onload = resolve;
+        head.appendChild(script);
+    });
+}
+
+loadScript('https://www.googletagmanager.com/gtag/js?id=G-D39YXHSX0V').then(() => {
+    // Initialize Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-D39YXHSX0V');
+});
 </script>
 """
 
-def inject_ga():
-    
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
-        index_path.write_text(new_html)
-
-inject_ga()
+components.html(
+    google_analytics_js,
+    height=0,
+    width=0
+)
