22624e802c64509e3c9f63cc5f72672053f0e00f
eselimnl <eselimnl@gmail.com>
2024-05-08 22:55:37 +0100

    revert

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 import streamlit as st
+# GA
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
 
-st.title('My ESU Streamlit app 3')
+st.title('My ESU Streamlit app 2')
 
 user_input = st.text_input("Enter some text")
 st.write('The user entered:', user_input)
 
-google_analytics_js = """
-<!-- Global site tag (gtag.js) - Google Analytics -->
+GA_ID = "google_analytics"
+GA_SCRIPT = """
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-D39YXHSX0V"></script>
-<script>
+<script id='google_analytics'>
   window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-D39YXHSX0V');
 </script>
 """
 
-# Embed Google Analytics script within iframe HTML content
-iframe_content = f"""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Streamlit App</title>
-  {google_analytics_js}
-</head>
-<body>
-  <iframe src="https://912-app-test-2mzhv9bgobnsxgnuga9n7e.streamlit.app/" width="100%" height="100%" frameborder="0"></iframe>
-</body>
-</html>
-"""
+def inject_ga():
+    
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
+        index_path.write_text(new_html)
 
-st.components.v1.iframe(iframe_content, height=1, scrolling=False)
+inject_ga()
