1ca7841382af0c2d2e6bfa9c87aa9bbb342c83db
eselimnl <eselimnl@gmail.com>
2024-05-08 20:40:02 +0100

    test34

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('My ESU Streamlit app 2')
+st.title('My ESU Streamlit app 3')
 
 user_input = st.text_input("Enter some text")
 st.write('The user entered:', user_input)
