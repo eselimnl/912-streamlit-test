8880b7750ca9a6b76d136d05f870080fb2ae9be2
eselimnl <eselimnl@gmail.com>
2024-05-10 14:56:11 +0100

    statcounter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 # Function to inject Google Analytics script
 def inject_google_analytics():
@@ -31,3 +32,23 @@ def main():
 # Entry point of the app
 if __name__ == "__main__":
     main()
+#this code below is the statcounter tracking code
+statcounter= """
+
+    <!-- Default Statcounter code for testto
+https://912-app-test-2mzhv9bgobnsxgnuga9n7e.streamlit.app/ -->
+<script type="text/javascript">
+var sc_project=12997381; 
+var sc_invisible=1; 
+var sc_security="8ce07403"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12997381/0/8ce07403/1/" alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code --
+"""
+
+components.html(statcounter,width=200, height=200)   
