adae0055a98a8042477fe25076a07c7a688c6761
eselimnl <eselimnl@gmail.com>
2024-05-11 12:15:29 +0100

    cookie banner

--- app.py
+++ app.py
@@ -1,24 +1,58 @@
 import streamlit as st
-import streamlit.components.v1 as components
-
+from streamlit.components.v1 import html
+from streamlit_modal import Modal
 #this code below is the statcounter tracking code
-statcounter= """
 
+def statcounter():
+
+    statcounter_script= """
     <!-- Default Statcounter code for testto
-https://912-app-test-2mzhv9bgobnsxgnuga9n7e.streamlit.app/ -->
-<script type="text/javascript">
-var sc_project=12997381; 
-var sc_invisible=1; 
-var sc_security="8ce07403"; 
+    https://912-app-test-2mzhv9bgobnsxgnuga9n7e.streamlit.app/ -->
+    <script type="text/javascript">
+    var sc_project=12997381; 
+    var sc_invisible=1; 
+    var sc_security="8ce07403"; 
+    </script>
+    <script type="text/javascript"
+    src="https://www.statcounter.com/counter/counter.js" async></script>
+    <noscript><div class="statcounter"><a title="Web Analytics"
+    href="https://statcounter.com/" target="_blank"><img class="statcounter"
+    src="https://c.statcounter.com/12997381/0/8ce07403/1/" alt="Web Analytics"
+    referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+    <!-- End of Statcounter Code --
+    """
+    components.html(statcounter,width=200, height=200)  
+
+cookie_banner = """
+<div id="cookie-banner" style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #f1f1f1; padding: 10px; text-align: center;">
+    This website uses cookies to ensure you get the best experience on our website.
+    <button onclick="acceptCookies()" style="margin-left: 10px;">Accept</button>
+    <button onclick="rejectCookies()" style="margin-left: 10px;">Decline</button>
+</div>
+
+<script>
+function acceptCookies() {
+    // Add Google Analytics tag
+    var gaScript = document.createElement('script');
+    gaScript.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-D39YXHSX0V');
+    `;
+    document.head.appendChild(gaScript);
+
+    document.getElementById('cookie-banner').style.display = 'none';
+}
+
+function rejectCookies() {
+    document.getElementById('cookie-banner').style.display = 'none';
+}
 </script>
-<script type="text/javascript"
-src="https://www.statcounter.com/counter/counter.js" async></script>
-<noscript><div class="statcounter"><a title="Web Analytics"
-href="https://statcounter.com/" target="_blank"><img class="statcounter"
-src="https://c.statcounter.com/12997381/0/8ce07403/1/" alt="Web Analytics"
-referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
-<!-- End of Statcounter Code --
 """
+
+html(cookie_banner)
+
 # Main Streamlit app
 def main():
     st.title('My Streamlit App for myselfESU')
@@ -26,7 +60,7 @@ def main():
     # Inject Google Analytics script
     # Your app content goes here
     st.write("Welcome to my Streamlit app!")
-    components.html(statcounter,width=200, height=200)  
+    #statcounter()
 # Entry point of the app
 if __name__ == "__main__":
     main()
